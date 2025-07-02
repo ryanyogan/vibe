3fc59eb5b91a8352a66b8f9dc4a42bb2e35b5e42
Ryan Yogan <ryanyogan@gmail.com>
2025-07-02 12:15:02 -0500

    Initial Project Creation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,4 @@
-import * as schema from "~/database/schema";
-
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,47 +7,16 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function action({ request, context }: Route.ActionArgs) {
-  const formData = await request.formData();
-  let name = formData.get("name");
-  let email = formData.get("email");
-  if (typeof name !== "string" || typeof email !== "string") {
-    return { guestBookError: "Name and email are required" };
-  }
-
-  name = name.trim();
-  email = email.trim();
-  if (!name || !email) {
-    return { guestBookError: "Name and email are required" };
-  }
-
-  try {
-    await context.db.insert(schema.guestBook).values({ name, email });
-  } catch (error) {
-    return { guestBookError: "Error adding to guest book" };
-  }
-}
-
 export async function loader({ context }: Route.LoaderArgs) {
-  const guestBook = await context.db.query.guestBook.findMany({
-    columns: {
-      id: true,
-      name: true,
-    },
-  });
-
   return {
-    guestBook,
     message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE,
   };
 }
 
 export default function Home({ actionData, loaderData }: Route.ComponentProps) {
   return (
-    <Welcome
-      guestBook={loaderData.guestBook}
-      guestBookError={actionData?.guestBookError}
-      message={loaderData.message}
-    />
+    <div>
+      <h1>{loaderData.message}</h1>
+    </div>
   );
 }
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "732ff21e-318f-4cb2-98f7-df96e895d30f",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
